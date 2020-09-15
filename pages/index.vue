826d4ef24381fba308c1fe5a0b6343e9fe09d1c3
Juke-99 <yanpahika@gmail.com>
2020-09-16 08:57:21 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<style scoped>
+  html, body{
+    padding: 0;
+    margin: 0;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  div {
+    max-width: 870px;
+    margin: auto;
+  }
+</style>
+
+<template>
+  <div>
+    <h1>Mechanics</h1>
+    <nuxt-link to='./chapter1/the_equations_of_motion'><h2>Chapter1 - 運動方程式</h2></nuxt-link>
+  </div>
+</template>
